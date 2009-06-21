299685d39bd53d6b17798039367a2eeae304ddf9
Jan <info@teilnehmer.ws>
2009-06-21 12:37:31 +0200

    some new lines.

--- hw.rb
+++ hw.rb
@@ -1,2 +1,22 @@
-puts "Hello World."
-puts 1+2+3
+# A test file.
+# =====================================
+# Very basic outputting
+puts 'Seconds in 1 year:'
+puts 365*24*60*60
+
+# Variables
+myString = 'Hello World'
+puts myString
+hr = '============================='
+puts hr
+
+# Changing variable types
+number = 42
+string = '12'
+# ".to_s" => treat as string
+#  "to_i" => treat as integer
+puts "String Magic: "
+puts number.to_s + string
+puts "Number Magic: "
+puts number + string.to_i
+
